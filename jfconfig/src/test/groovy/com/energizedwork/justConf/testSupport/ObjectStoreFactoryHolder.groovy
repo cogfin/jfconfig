17916d847b1da1646baf6b572b5f973fc6d978f5
Andrew Eacott <andrew.eacott@skyscanner.net>
2019-06-14 12:47:41 +0100

    * Fix import JVM internal class (com.sun.istack.internal.NotNull) * Add compatibility for Dropwizard 1.3.x * Added tests for Dropwizard 1.3.12

--- jfconfig/src/test/groovy/com/energizedwork/justConf/testSupport/ObjectStoreFactoryHolder.groovy
+++ jfconfig/src/test/groovy/com/energizedwork/justConf/testSupport/ObjectStoreFactoryHolder.groovy
@@ -3,7 +3,7 @@ package com.energizedwork.justConf.testSupport
 import com.energizedwork.justConf.testSupport.objectStore.ObjectStoreFactory
 import com.fasterxml.jackson.annotation.JsonIdentityInfo
 import com.fasterxml.jackson.annotation.ObjectIdGenerators
-import com.sun.istack.internal.NotNull
+import javax.validation.constraints.NotNull;
 
 import javax.validation.Valid
 
